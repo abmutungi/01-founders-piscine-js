06ac6a37ac610921941e72e1fa714febfa5fe8cb
abmutungi <abmutungi@gmail.com>
2022-06-10 12:36:47 +0100

    nasa

--- nasa.js
+++ nasa.js
@@ -4,10 +4,10 @@ const nasa = (num) => {
   for (let i = 1; i <= num; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       str += "NASA" + " ";
-    } else if (i % 3 === 0 && i % 5 != 0) {
-      str += "NA" + " ";
     } else if (i % 3 != 0 && i % 5 === 0) {
       str += "SA" + " ";
+    } else if (i % 3 === 0 && i % 5 != 0) {
+      str += "NA" + " ";
     } else {
       str += i + " ";
     }
