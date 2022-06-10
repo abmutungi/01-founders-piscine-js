3ec2a9e8ef4f17bf11ef45075f33a80d0dd1e79f
abmutungi <abmutungi@gmail.com>
2022-06-10 12:57:30 +0100

    molecule-cells

---
+++ molecules-cells.js
@@ -0,0 +1,32 @@
+const RNA = (str) => {
+  res = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "G") {
+      res += "C";
+    } else if (str[i] === "C") {
+      res += "G";
+    } else if (str[i] === "T") {
+      res += "A";
+    } else if (str[i] === "A") {
+      res += "U";
+    } else res += str[i];
+  }
+  return res;
+};
+
+const DNA = (str) => {
+  res = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "C") {
+      res += "G";
+    } else if (str[i] === "G") {
+      res += "C";
+    } else if (str[i] === "A") {
+      res += "T";
+    } else if (str[i] === "U") {
+      res += "A";
+    } else res += str[i];
+  }
+  return res;
+};
+console.log(RNA(DNA("AUCG")));
