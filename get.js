b53d6fa0811b8ead7d9ef79a89258e0df977d8ce
abmutungi <abmutungi@gmail.com>
2022-06-10 13:43:57 +0100

    get

---
+++ get.js
@@ -0,0 +1,17 @@
+// const src = { nested: { key: "peekaboo" } };
+// const path = "nested.key";
+
+const get = (src, path) => {
+  let pArr = path.split(".");
+  let res = src;
+
+  pArr.forEach((element) => {
+    res = res[element];
+  });
+
+  return res;
+};
+
+// console.log(path.split("."));
+// console.log(src);
+console.log(get(src, path));
