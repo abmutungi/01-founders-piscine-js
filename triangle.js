6b973f5ab3c9985c2cbd2e3319e33e0e264b628d
abmutungi <abmutungi@gmail.com>
2022-06-10 14:31:55 +0100

    triangle

---
+++ triangle.js
@@ -0,0 +1,12 @@
+const triangle = (str, num) => {
+  let res = "";
+  for (let i = 0; i <= num; i++) {
+    for (let j = 0; j < i; j++) {
+      res += str;
+    }
+    res += str + "\n";
+  }
+  return res.trim();
+};
+console.log(triangle("*", 14));
+// console.log(line);
