5e802d4e050487f6742cf8c3697c490e18b6d35e
abmutungi <abmutungi@gmail.com>
2022-06-13 19:52:33 +0100

    date-is

--- date-is.js
+++ date-is.js
@@ -1,9 +1,39 @@
-const isValid = (date) => {};
+const isValid = (date) => {
+  if (typeof date !== "string") {
+    date = new Date(date);
+  }
+  if (
+    date instanceof Date &&
+    !isNaN(date) &&
+    date.toString() !== "Invalid Date"
+  ) {
+    return true;
+  }
+  return false;
+};
 const isAfter = (d1, d2) => {
   if (new Date(d1) > new Date(d2)) {
     return true;
   }
   return false;
 };
+const isBefore = (d1, d2) => {
+  if (isValid(d1) && isValid(d2) && !isAfter(d1, d2)) {
+    return true;
+  }
+};
 
-console.log(isAfter(new Date("1992-01-01"), new Date("1992-01-02")));
+const isFuture = (date) => {
+  if (isValid(date) && isAfter(date, Date.now())) {
+    return true;
+  }
+  return false;
+};
+
+const isPast = (date) => {
+  if (isValid(date) && !isFuture(date)) {
+    return true;
+  }
+  return false;
+};
+console.log(isBefore(new Date("1992-01-01"), new Date("1992-01-02")));
