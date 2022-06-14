15365740d6f01c711994bd826b24d127845411fc
abmutungi <abmutungi@gmail.com>
2022-06-14 16:58:38 +0100

    match-cron

--- match-cron.js
+++ match-cron.js
@@ -1,25 +1,24 @@
 const matchCron = (cron, date) => {
   let res = [];
-  //   let regexp = /\d/g;
-  //   if (!regexp.test(cron)) {
-  //     return new Date();
-  //   } else {
-  if (cron[0] == date.getMinutes()) {
-    res.push(1);
-  } else if (cron[2] == date.getHours()) {
-    res.push(1);
-  } else if (cron[4] == date.getDate()) {
-    res.push(1);
-  } else if (cron[6] == date.getMonth()) {
-    res.push(1);
-  } else if (cron[8] == date.getDay()) {
-    res.push(1);
+  let regexp = /\d/g;
+  if (!regexp.test(cron)) {
+    return new Date();
+  } else {
+    if (cron[0] == date.getMinutes()) {
+      res.push(1);
+    } else if (cron[2] == date.getHours()) {
+      res.push(1);
+    } else if (cron[4] == date.getDate()) {
+      res.push(1);
+    } else if (cron[6] == date.getMonth() + 1) {
+      res.push(1);
+    } else if (cron[8] == date.getDay()) {
+      res.push(1);
+    }
+    if (!res.includes(1)) {
+      return false;
+    }
+    return true;
   }
-
-  if (!res.includes(1)) {
-    return false;
-  }
-  return true;
 };
-
-console.log(matchCron('* * * * 1', new Date('2020-06-02 00:00:00')));
+console.log(matchCron("3 3 * 3 3", new Date("2021-03-02 03:03:00")));
