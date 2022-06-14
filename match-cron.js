28533d03db99327b329e4775cba37c64799925fc
abmutungi <abmutungi@gmail.com>
2022-06-14 17:21:44 +0100

    matchcron

--- match-cron.js
+++ match-cron.js
@@ -1,23 +1,23 @@
 const matchCron = (cron, date) => {
-  let res = [];
+  //let res = [];
   let regexp = /\d/g;
   if (!regexp.test(cron)) {
     return new Date();
   } else {
-    if (cron[0] == date.getMinutes()) {
-      res.push(1);
-    } else if (cron[2] == date.getHours()) {
-      res.push(1);
-    } else if (cron[4] == date.getDate()) {
-      res.push(1);
-    } else if (cron[6] == date.getMonth() + 1) {
-      res.push(1);
-    } else if (cron[8] == date.getDay()) {
-      res.push(1);
-    }
-    if (!res.includes(1)) {
+    if (cron[0] != date.getMinutes() && cron[0] != "*") {
+      return false;
+    } else if (cron[2] != date.getHours() && cron[2] != "*") {
+      return false;
+    } else if (cron[4] != date.getDate() && cron[4] != "*") {
+      return false;
+    } else if (cron[6] != date.getMonth() + 1 && cron[6] != "*") {
+      return false;
+    } else if (cron[8] != date.getDay() && cron[8] != "*") {
       return false;
     }
+    // if (!res.includes(1)) {
+    //   return false;
+    // }
     return true;
   }
 };
