a9be91eb1ef00e880ba66307bd1b49f2b2a398bb
abmutungi <abmutungi@gmail.com>
2022-06-14 14:07:40 +0100

    rebecca-black

---
+++ rebecca-black.js
@@ -0,0 +1,25 @@
+const isValid = (date) => {
+  if (typeof date !== "string") {
+    date = new Date(date);
+  }
+  if (
+    date instanceof Date &&
+    !isNaN(date) &&
+    date.toString() !== "Invalid Date"
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const isFriday = (day) => (new Date(day).getDay() === 5 ? true : false);
+const isWeekend = (day) => (new Date(day).getDay() === 0 || new Date(day).getDay() === 6 ? true : false);
+const isLeapYear = (year) => new Date(year).getFullYear() % 4 === 0 ? true : false;
+function isLastDayOfMonth(date){
+    var test = new Date(date.getTime()),
+        month = test.getMonth();
+
+    test.setDate(test.getDate() + 1);
+    return test.getMonth() !== month;
+}
+console.log(isLastDayOfMonth(new Date("2020-02-29")));
