64906678b2b68749f8454dfea3cface96441073b
abmutungi <abmutungi@gmail.com>
2022-06-15 14:34:07 +0100

    using-reduce

---
+++ using-reduce.js
@@ -0,0 +1,4 @@
+const adder = (arr, start = 0) => arr.reduce((acc, val) => acc + val) + start;
+const sumOrMul = (arr, elem = 0) =>
+  arr.reduce((acc, val) => (val % 2 == 0 ? acc * val : acc + val), elem);
+const funcExec = (func, elem = 0) => func.reduce((acc, val) => val(acc), elem);
