9d58169609cc9dfec0079dcff5b3d7b0aa3b0513
abmutungi <abmutungi@gmail.com>
2022-06-16 12:15:14 +0100

    sweet-curry

---
+++ sweet-curry.js
@@ -0,0 +1,17 @@
+const mult2 = (num) => {
+  mult2(num2);
+  return num * num2;
+};
+
+const add3 = (a) => {
+  add3(b);
+  add3(c);
+  return a + b + c;
+};
+
+const sub4 = (a) => {
+  sub4(b);
+  sub4(c);
+  sub4(d);
+  return a - b - c - d;
+};
